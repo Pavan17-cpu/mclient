fed601441bb3f408c4e6a85e680efc810baee850
Pavan17-cpu <110300098+Pavan17-cpu@users.noreply.github.com>
2024-03-19 18:21:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css'; // Import your CSS file for styling
 
 function VerificationPage({ onVerificationSuccess }) {
-  const [code, setCode] = useState('');
-  const [verificationError, setVerificationError] = useState('');
+  const [verificationCode, setVerificationCode] = useState(Array(10).fill(''));
+
+  const handleChange = (index, value) => {
+    const newVerificationCode = [...verificationCode];
+    newVerificationCode[index] = value;
+    setVerificationCode(newVerificationCode);
+    if (value && index < 9) {
+      // Automatically focus on the next input field if a value is entered
+      document.getElementById(`codeInput-${index + 1}`).focus();
+    }
+  };
 
   const handleVerification = async () => {
+    const code = verificationCode.join(''); // Concatenate all the code values
     try {
       const response = await axios.post('https://mserver-lzs3.onrender.com/verify', { code });
       if (response.data.success) {
         onVerificationSuccess();
       } else {
-        setVerificationError('Incorrect code. Please try again.');
+        console.error('Incorrect code. Please try again.');
       }
     } catch (error) {
       console.error('Error verifying code:', error);
-      setVerificationError('Error verifying code.');
     }
   };
 
   return (
-    <div>
-      <label htmlFor="codeInput">CODE:</label>
-      <input
-        type="password"
-        id="codeInput"
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-        maxLength={10}
-      />
-      <button onClick={handleVerification}>Verify</button>
-      {verificationError && <div>{verificationError}</div>}
+    <div className="center-container">
+      <div className="verification-container">
+        <label>Enter Verification Code:</label>
+        <div className="otp-container">
+          {verificationCode.map((value, index) => (
+            <input
+              key={index}
+              id={`codeInput-${index}`}
+              type="password"
+              maxLength="1"
+              value={value}
+              onChange={(e) => handleChange(index, e.target.value)}
+            />
+          ))}
+        </div>
+        <button onClick={handleVerification}>Verify</button>
+      </div>
     </div>
   );
 }
@@ -44,10 +58,10 @@ function App() {
   const [verified, setVerified] = useState(false);
 
   useEffect(() => {
-    if (viewGallery) {
+    if (viewGallery && verified) {
       fetchImagesVideos();
     }
-  }, [viewGallery]);
+  }, [viewGallery, verified]);
 
   const fetchImagesVideos = async () => {
     try {
@@ -84,38 +98,45 @@ function App() {
   };
 
   return (
-    <div>
-      {!verified && <VerificationPage onVerificationSuccess={handleVerificationSuccess} />}
+    <div className="background-gradient">
+      {!verified && (
+        <VerificationPage onVerificationSuccess={handleVerificationSuccess} />
+      )}
       {verified && !viewGallery && (
-        <div>
-          <input type="file" onChange={handleFileChange} />
-          <button onClick={handleUpload}>Upload</button>
-          <button onClick={() => setViewGallery(true)}>View Gallery</button>
+        <div className="center-container">
+          <div>
+            <input type="file" accept="image/*, video/*" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload</button>
+            <button onClick={() => setViewGallery(true)}>View Gallery</button>
+            <button onClick={() => setVerified(false)}>Back to Home</button>
+          </div>
         </div>
       )}
       {viewGallery && (
-        <div>
-          <button onClick={() => setViewGallery(false)}>Back to Upload</button>
-          <div className="gallery-container">
-            {imagesVideos.map((item, index) => (
-              <div key={index} className="card">
-                {item.contentType.startsWith('image') ? (
+        
+        <div className="gallery-container">
+           <button onClick={() => setViewGallery(false)} className="back-to-upload">Back to Upload</button>
+          {imagesVideos.map((item, index) => (
+            <div key={index} className="card">
+              {item.contentType.startsWith('image') ? (
+                <div className="image-container">
                   <img src={`data:${item.contentType};base64,${item.data}`} alt={`Image ${index}`} />
-                ) : (
-                  <video controls className="video">
-                    {['mp4', 'mov'].map((format, formatIndex) => (
-                      <source
-                        key={formatIndex}
-                        src={`data:${item.contentType};base64,${item.data}`}
-                        type={`video/${format}`}
-                      />
-                    ))}
-                    Your browser does not support the video tag.
-                  </video>
-                )}
-              </div>
-            ))}
-          </div>
+                </div>
+              ) : (
+                <video controls className="video">
+                  {['mp4', 'mov'].map((format, formatIndex) => (
+                    <source
+                      key={formatIndex}
+                      src={`data:${item.contentType};base64,${item.data}`}
+                      type={`video/${format}`}
+                    />
+                  ))}
+                  Your browser does not support the video tag.
+                </video>
+              )}
+            </div>
+          ))}
+         
         </div>
       )}
     </div>
